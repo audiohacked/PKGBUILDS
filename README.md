b8ba8a14ecd45cc9e6054e5f0f36eea80bfe8496
Sean Nelson <audiohacked@gmail.com>
2014-07-25 11:18:05 -0600

    add craftbukkit packages, and update README

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# ArchLinux-CMaNGOS
+# ArchLinux PKGBUILDs for craftbukkit and CMaNGOS
 
 This Repository is for the PKGBUILDs to build C(ontinued)-MaNGOS server and install the databases.
 You can visit the Upstream Code at [CMaNGOS](https://github.com/cmangos).
 
 *NOTE*: the only packages that currently work are cmangos-cata-git and cmangos-wotlk-git!
+
+CraftBukkit is a Minecraft Mod Server. [Bukkit](https://bukkit.org/) is the place to get the server and plugins.
+These packages are modified from the AUR versions in that you can run multiple versions of the Server concurrently.
+
+Craftbukkit-1.7.2 only exists because my first install was version 1.7.2 and I use it for my data and plugins that 
+only support version 1.7.2.
 
---
+++ craftbukkit-beta/craftbukkit.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+MAINPATH="/srv/craftbukkit-beta"
+BACKUPPATH="/srv/craftbukkit-beta/backup"
+WORLDPATH="/srv/craftbukkit-beta/world"
+
+USER="$2"
+SESSION="$3"
+
+if [ -z $2 ]; then
+        USER="craftbukkit"
+fi
+
+if [ -z $3 ]; then
+	SESSION="craftbukkit-beta-console"
+fi
+
+save_stop() {
+    sudo -u ${USER} tmux send-keys -t ${SESSION} 'save-off' C-m
+    sudo -u ${USER} tmux send-keys -t ${SESSION} 'save-all' C-m
+}
+
+save_start() {
+    sudo -u ${USER} tmux send-keys -t ${SESSION} 'save-on' C-m
+}
+
+case "$1" in
+  start)
+    if ! sudo -u ${USER} tmux has -t ${SESSION}; then
+      sudo -u ${USER} tmux new-session -d -s ${SESSION} -d "cd ${MAINPATH}; java -Xmx1024M -Xms1024M -jar ${MAINPATH}/craftbukkit.jar nogui"
+      if [ $? -gt 0 ]; then
+        exit 1
+      fi
+    else
+      echo "Craftbukkit already started"
+      exit 1
+    fi
+    ;;
+
+  stop)
+    sudo -u ${USER} tmux send-keys -t ${SESSION} 'broadcast NOTICE: Server shutting down in 5 seconds!' C-m
+    sleep 5
+    sudo -u ${USER} tmux send-keys -t ${SESSION} 'stop' C-m
+    sleep 10
+    ;;
+
+  console)
+    sudo -u ${USER} tmux attach -t ${SESSION}
+    ;;
+  
+  backup)
+    echo "Starting backup"
+    FILE="`date +%Y%m%d%H%M`.tar.gz"
+    path="$BACKUPPATH/$FILE"
+    su -s /bin/bash -c "mkdir -p $BACKUPPATH" ${USER}
+    save_stop
+    su -s /bin/bash -c "tar -czf $path $WORLDPATH" ${USER}
+    save_start
+    echo "Backup finished"
+    ;;
+
+  *)
+    echo "usage: $0 {start|backup|console} user"
+esac
+
+exit 0
+
